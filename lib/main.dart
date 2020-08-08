8c3a573931902812683fa18059e4bcd74445095b
RamonGiovane <ramon.giovane@gmail.com>
2020-08-08 13:30:33 -0300

    Lesson 206: Navigating product details using  routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/products_overview_screen.dart';
+import 'package:shop/widgets/product_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,9 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Minha Loja',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
       home: ProductOverviewScreen(),
+
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -8,35 +9,47 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover, //ocupa todo o tamanho do componente
-      ),
-
-      //Barra de informações
-      footer: GridTileBar(
-        backgroundColor: Colors.black87,
-       
-        //Botão de favorito
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
+    return ClipRRect(
+      //faz uma borda arredondada no tile
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              AppRoutes.PRODUCT_DETAIL,
+              arguments: product,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover, //ocupa todo o tamanho do componente
+          ),
         ),
 
-        //Nome do produto
-        title: Text(
-          product.title,
-          textAlign: TextAlign.center,
+        //Barra de informações
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+
+          //Botão de favorito
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+
+          //Nome do produto
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+
+          //Botão de carrinho
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
         ),
-
-        //Botão de carrinho
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
-        ), 
-
-
       ),
     );
   }
